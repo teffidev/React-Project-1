7ec7df758a2d892933a3273b5918f9f25b9546c1
teffidev <tefyberal09@gmail.com>
2023-03-07 22:59:04 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Card from './Components/Card/Card'
+import './App.css'
+import User from './Components/User/User'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Card/>
+      <User/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,11 @@
+import estilos from './Card.module.css'
+
+const Card = () => {
+    return (
+        <>
+        <h1 style={estilos.h1}>Hola, soy una tarjeta!</h1>
+        </>
+    );
+};
+
+export default Card;
